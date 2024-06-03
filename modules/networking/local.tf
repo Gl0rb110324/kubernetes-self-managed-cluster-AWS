630e8c855f3a5929073a59cac29fe5ba0f93991f
chung tran <chung.tran@cgi.com>
2024-06-03 11:18:31 +0100

    nat gateways

--- modules/networking/local.tf
+++ modules/networking/local.tf
@@ -3,7 +3,7 @@ locals {
   cluster_subnets_list = cidrsubnets(local.cluster_vpc_cidr, 8, 8, 8, 8, 8, 8) #3 public. 3 private
   subnet_regions       = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   subnet_name_notation = ["A", "B", "C"]
-  endpoints = {
+  endpoints = { ##endpoints for ssm tunnelling allowing access to the nodes
     "endpoint-ssm" = {
       name = "ssm"
     },
